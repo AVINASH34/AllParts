a6464c70dab773d369d9f9fc0e6b6f9be4fbeb3b
Avinash <86154551+AVINASH34@users.noreply.github.com>
2024-01-12 16:14:36 +0530

    Update My_Listeners.java

--- src/main/java/Reusable_methods/My_Listeners.java
+++ src/main/java/Reusable_methods/My_Listeners.java
@@ -2,39 +2,33 @@ package Reusable_methods;
 
 import org.testng.*;
 
+public class My_Listeners implements ITestListener {
 
-public class My_Listeners implements ITestListener{
+    public void onTestStart(ITestResult result) {
+        System.out.println("OnTestStart");
+    }
 
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("OnTestSuccess");
+    }
 
-	public void onTestStart(ITestResult result) {
-		System.out.println("OnTestStarted");
-	}
+    public void onTestFailure(ITestResult result) {
+        System.out.println("OnTestFailure");
+    }
 
-	public void onTestSuccess(ITestResult result) {
-		System.out.println("OnTestSucess");
-	}
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("OnTestSkipped");
+    }
 
-	public void onTestFailure(ITestResult result) {
-		System.out.println("OnTestFail");
-	}
+    public void onTestFailedWithTimeout(ITestResult result) {
+        System.out.println("OnTestFailedWithTimeout");
+    }
 
-	public void onTestSkipped(ITestResult result) {
-		System.out.println("OnTestSkipped");
-	}
+    public void onStart(ITestContext context) {
+        System.out.println("OnStart");
+    }
 
-	public void onTestFailedWithTimeout(ITestResult result) {
-		System.out.println("OnTestFailwithTimeOut");
-	}	
-
-	public void onStart(ITestContext context) {
-		System.out.println("OnTestStart");
-	}
-	
-
-	public void onFinish(ITestContext context) {
-		System.out.println("OnTestFinish");
-	}
-	
-	
-
+    public void onFinish(ITestContext context) {
+        System.out.println("OnFinish");
+    }
 }
